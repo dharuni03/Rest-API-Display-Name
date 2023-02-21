bd24b0b04898ea932eb3a5b748acd5107d97ecbe
dharuni03 <727721euit030@skcet.ac.in>
2023-02-21 12:30:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/DharuniApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DharuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DharuniApplication.class, args);
+	}
+
+}
